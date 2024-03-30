ee8e6a009c6e8b917be65bfd4aa6595ccfa1c297
jay vekariya <vekariya.ja@northeastern.edu>
2024-03-29 23:32:36 -0400

    fix: resume not found

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -30,9 +30,10 @@ export const Hero = () => {
       </p>
       <Link
         target="_blank"
-        href="/resume.pdf"
+        href={'public/resume.pdf'}
         className="px-4 py-2 mt-3 bg-neutral-50 text-neutral-950 rounded-lg md:text-lg font-bold"
         download={true}
+        rel={'noreferrer noopener'}
         locale={false}
       >
         Resume
